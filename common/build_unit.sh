5808c2a6a5a97cd8ad9851886fa4e6325e311072
Martin Krastev <blu.dark@gmail.com>
2021-01-17 16:22:24 +0200

    * clean up unittest build script * clean up testvect_native build script

--- common/build_unit.sh
+++ common/build_unit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # known issue with clang++-3.6 and the lifespan of temporaries, causing multiple failures at unittest
-CC=${CXX:-clang++-10}
+CXX=${CXX:-clang++}
 BINARY=unittest
 SOURCE=(
 	unittest.cpp
 )
-CFLAGS=(
+CXXFLAGS=(
 # g++-4.8 through 4.9 suffer from a name-mangling conflict in cephes headers; switching to the following ABI version resolves that
 #	-fabi-version=6
 # g++-6 warns about ignored attributes in template arguments (e.g. __m256 as a typename template arg to simd::native8)
@@ -23,54 +23,63 @@ CFLAGS=(
 # Compiler quirk 0001: control definition location of routines posing entry points to recursion for more efficient inlining
 #	-DCLANG_QUIRK_0001=1
 )
-# For non-native or tweaked architecture targets, comment out 'native' and uncomment the correct target architecture and flags
-TARGET=(
-	native
-	native
-# AMD Bobcat:
-#	btver1
-#	btver1
-# AMD Jaguar:
-#	btver2
-#	btver2
-# note: Jaguars have 4-wide SIMD, so our avx256 code is not beneficial to them
-#	-mno-avx
-# Intel Core2
-#	core2
-#	core2
-# Intel Nehalem
-#	corei7
-#	corei7
-# Intel Sandy Bridge
-#	corei7-avx
-#	corei7-avx
-# Intel Ivy Bridge
-#	core-avx-i
-#	core-avx-i
-# Arm Cortex-A76
-#	armv8.2-a
-#	cortex-a76
-)
+
+source cxx_util.sh
+
+if [[ ${MACHTYPE} =~ "-apple-darwin" ]]; then
+	if [[ ${HOSTTYPE} == "arm64" ]]; then
+
+		CXXFLAGS+=(
+			-march=armv8.4-a
+		)
+
+	elif [[ ${HOSTTYPE} == "x86_64" ]]; then
+
+		CXXFLAGS+=(
+			-march=native
+		)
+
+	fi
+	CXXFLAGS+=(
+		-mtune=native
+		-DCACHELINE_SIZE=`sysctl hw.cachelinesize | sed 's/^hw.cachelinesize: //g'`
+	)
+
+elif [[ ${MACHTYPE} =~ "-linux-" ]]; then
+	if [[ ${HOSTTYPE} == "arm64" || ${HOSTTYPE} == "aarch64" ]]; then
+
+		cxx_uarch_arm
+
+	elif [[ ${HOSTTYPE} == "x86_64" ]]; then
+
+		CXXFLAGS+=(
+			-march=native
+			-mtune=native
+			-DCACHELINE_SIZE=64
+		)
+	fi
+else
+	echo Unknown platform
+	exit 255
+fi
+
 LFLAGS=(
 # Alias some glibc6 symbols to older ones for better portability
 #	-Wa,-defsym,memcpy=memcpy@GLIBC_2.2.5
 #	-Wa,-defsym,__sqrtf_finite=__sqrtf_finite@GLIBC_2.2.5
 	-lstdc++
 	-ldl
-	-lrt
 )
 
 if [[ $1 == "debug" ]]; then
-	CFLAGS+=(
+	CXXFLAGS+=(
 		-Wall
 		-O0
 		-g
-		-fstandalone-debug
 		-DDEBUG
 	)
 else
-	CFLAGS+=(
-# Enable some optimisations that may or may not be enabled by the global optimisation level of choice in this compiler version
+	CXXFLAGS+=(
 		-ffast-math
 		-fno-unsafe-math-optimizations
 		-fstrict-aliasing
@@ -83,6 +92,6 @@ else
 	)
 fi
 
-BUILD_CMD=$CC" -o "$BINARY" "${CFLAGS[@]}" -march="${TARGET[0]}" -mtune="${TARGET[@]:1}" "${SOURCE[@]}" "${LFLAGS[@]}
-echo $BUILD_CMD
-CCC_ANALYZER_CPLUSPLUS=1 $BUILD_CMD
+BUILD_CMD=${CXX}" -o "${BINARY}" "${CXXFLAGS[@]}" "${SOURCE[@]}" "${LFLAGS[@]}
+echo ${BUILD_CMD}
+CCC_ANALYZER_CPLUSPLUS=1 ${BUILD_CMD}
